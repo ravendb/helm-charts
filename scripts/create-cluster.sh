62826b5b9c439a492d79839180d1e744224006bb
Gracjan Sadowicz <gracjan@ravendb.net>
2022-04-04 09:52:54 +0200

    Stateful sets, cluster, sidecar

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
- 
+#!bin/bash
+apt-get update
+apt-get install curl -y
+uriB='http://ravendb-1:8080/admin/cluster/node?url=http%3A%2F%2Fravendb-0%3A8080&watcher=false&tag=A' # B->A
+uriC='http://ravendb-1:8080/admin/cluster/node?url=http%3A%2F%2Fravendb-2%3A8080&watcher=false&tag=C' # B->C
+sleep 10 # wait for nodes to stand-up
+curl -L -X PUT $uriB -d ''
+curl -L -X PUT $uriC -d ''
