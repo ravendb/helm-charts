4a325b4f3705b6807ed5cbc1af51a036035f6021
Gracjan Sadowicz <gracjan@ravendb.net>
2022-04-14 08:24:51 +0200

    Http connection between nodes through ingress, tcp wip

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 apt-get update
 apt-get install curl -y
-uriB='http://ravendb-1:8080/admin/cluster/node?url=http%3A%2F%2Fravendb-0%3A8080&watcher=false&tag=A' # B->A
-uriC='http://ravendb-1:8080/admin/cluster/node?url=http%3A%2F%2Fravendb-2%3A8080&watcher=false&tag=C' # B->C
+uriB='http://a.poisson.development.run/admin/cluster/node?url=http%3A%2F%2Fb.poisson.development.run&watcher=false&tag=B' 
+uriC='http://a.poisson.development.run/admin/cluster/node?url=http%3A%2F%2Fc.poisson.development.run&watcher=false&tag=C' 
 sleep 10 # wait for nodes to stand-up
 curl -L -X PUT $uriB -d ''
 curl -L -X PUT $uriC -d ''
