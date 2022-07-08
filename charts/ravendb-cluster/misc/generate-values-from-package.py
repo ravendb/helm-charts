ce3865a3879f1113041c44462528fc74344e8041
Gracjan Sadowicz <gracjan@ravendb.net>
2022-07-08 09:45:38 +0200

    RavenDB-18848 Allow to use a single TCP port for all nodes

--- charts/ravendb-cluster/misc/generate-values-from-package.py
+++ charts/ravendb-cluster/misc/generate-values-from-package.py
@@ -37,9 +37,9 @@ CHART_PATH = ARGS.chart
 
 
 class NodeInfo:
-    def __init__(self, tag: str, tcp_port: int):
+    def __init__(self, tag: str): #, tcp_port: int):
         self.node_tag = tag
-        self.public_tcp_port = tcp_port
+        self.public_tcp_port = 443
 
     def to_yaml(self) -> Dict:
         return {"nodeTag": self.node_tag, "publicTcpPort": self.public_tcp_port}
@@ -119,9 +119,9 @@ def get_cluster_info_from_setup_package(
             continue
         with open(f"./setup_package/{file_name}/settings.json") as settings_json_ref:
             settings = json.loads(settings_json_ref.read())
-            port = settings["PublicServerUrl.Tcp"].split(":")[-1]
+            #port = settings["PublicServerUrl.Tcp"].split(":")[-1]
             node_tag = settings["PublicServerUrl.Tcp"].split("//")[1][0]
-            node_infos.append(NodeInfo(node_tag, port))
+            node_infos.append(NodeInfo(node_tag)) #, port))
             last_settings_json = settings
 
     print("Checking SetupMode and domain name..")
